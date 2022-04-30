51525ac96b83db4547cc32be90da6f9646d07cfb
JackLee9437 <iacog@kakao.com>
2022-04-30 10:48:23 +0900

    tree init implements

--- src/rbtree.c
+++ src/rbtree.c
@@ -2,43 +2,59 @@
 
 #include <stdlib.h>
 
-rbtree *new_rbtree(void) {
+rbtree *new_rbtree(void)
+{
   rbtree *p = (rbtree *)calloc(1, sizeof(rbtree));
+  p->nil = NULL;
+  p->root = NULL;
   // TODO: initialize struct if needed
   return p;
 }
 
-void delete_rbtree(rbtree *t) {
+void delete_node(node_t *p);
+
+void delete_rbtree(rbtree *t)
+{
   // TODO: reclaim the tree nodes's memory
   free(t);
 }
 
-node_t *rbtree_insert(rbtree *t, const key_t key) {
+void rotate_left(node_t *p);
+void rotate_right(node_t *p);
+void change_color(node_t *p);
+
+node_t *rbtree_insert(rbtree *t, const key_t key)
+{
   // TODO: implement insert
   return t->root;
 }
 
-node_t *rbtree_find(const rbtree *t, const key_t key) {
+node_t *rbtree_find(const rbtree *t, const key_t key)
+{
   // TODO: implement find
   return t->root;
 }
 
-node_t *rbtree_min(const rbtree *t) {
+node_t *rbtree_min(const rbtree *t)
+{
   // TODO: implement find
   return t->root;
 }
 
-node_t *rbtree_max(const rbtree *t) {
+node_t *rbtree_max(const rbtree *t)
+{
   // TODO: implement find
   return t->root;
 }
 
-int rbtree_erase(rbtree *t, node_t *p) {
+int rbtree_erase(rbtree *t, node_t *p)
+{
   // TODO: implement erase
   return 0;
 }
 
-int rbtree_to_array(const rbtree *t, key_t *arr, const size_t n) {
+int rbtree_to_array(const rbtree *t, key_t *arr, const size_t n)
+{
   // TODO: implement to_array
   return 0;
 }
